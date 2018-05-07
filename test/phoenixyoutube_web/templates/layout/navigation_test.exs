e1e8f9e8eb0ea9fb8809b7b0df968bb4ffd154f9
jthea <jthea0609@gmail.com>
2018-05-06 23:18:07 -0500

    Edit homepage, add navbar and custom errors

---
+++ test/phoenixyoutube_web/templates/layout/navigation_test.exs
@@ -0,0 +1,9 @@
+defmodule PhoenixyoutubeWeb.NavigationTest do
+  use PhoenixyoutubeWeb.ConnCase
+
+  test "shows a sign in with Google link when not signed in", %{conn: conn} do
+    conn = get conn, "/"
+    
+    assert html_response(conn, 200) =~ "Sign in with Google"
+  end
+end
